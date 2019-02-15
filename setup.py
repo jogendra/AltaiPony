12310fb7ddd061d28f4f130b529c213fd4b0883c
Jogendra Kumar <imjog24@gmail.com>
2019-02-15 18:39:32 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ setuptools.setup(
     description="A flare finding and analysis package for K2",
     long_description=long_description,
     long_description_content_type="text/restructuredtext",
-    url="https://github.com/ekaterinailin/AltaiPony",
+    url="https://github.com/jogendra/AltaiPony",
     packages=setuptools.find_packages(),
     install_requires = ['lightkurve>=1.0b21','numpy>=1.15.1', 'pandas>=0.23.4',
                         'progressbar2'],
-    dependency_links=['git+ssh://git@github.com/OxES/k2sc.git'],
+    dependency_links=['https://github.com/OxES/k2sc.git'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
